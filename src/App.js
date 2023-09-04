4a4d75dc8d53dc68f46fd27eeb74e824f977b6f5
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-09-05 00:04:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className='parent-box'>
+
+        <div className='result-box'>
+          <h1>Your result</h1>
+          <h2>Great</h2>
+          <h3>sjkdjaksjkdas kasdkjsajksajk</h3>
+
+
+         result
+        </div>
+
+        <div className='summary-box'>
+         Summary  
+        </div>
+
+
+      </div>
+       
+    </div>
+  );
+}
+
+export default App;
